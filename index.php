e8a4ec825ca596687ad444fa748ac662bb6884ec
Garvit Verma <garvit@bizongo.in>
2015-09-23 16:38:15 +0530

    Index file for getting url and email id of user

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>
+		</title>
+	</head>
+	<body>
+		<form method="post", action="add_for_script.php">
+			<p>
+				URL: 
+				<input type="text", name="url">
+			</p>
+			<p>
+				Email:
+				<input type="text", name="email">
+			</p>
+			<input type="submit">
+		</form>
+	</body>
+</html>
